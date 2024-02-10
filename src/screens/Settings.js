1f437013e6ab1bab06d6d4e0dc35163685f2ae69
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2024-02-11 08:26:17 +0900

    feat: 설정 페이지 추가

---
+++ src/screens/Settings.js
@@ -0,0 +1,58 @@
+import { useRef, useState } from "react";
+
+const Settings = () => {
+  const cigaretteInput = useRef();
+  const priceInput = useRef();
+  const [state, setState] = useState({
+    cigarette: "",
+    price: "",
+  });
+  const onChange = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const onSubmit = () => {
+    if (state.cigarette.length < 1) {
+      cigaretteInput.current.focus();
+      return;
+    }
+    if (state.price.length < 4) {
+      priceInput.current.focus();
+      return;
+    }
+    setState({
+      cigarette: "",
+      price: "",
+    });
+    alert("저장 성공");
+  };
+  return (
+    <div className="Settings">
+      <div>
+        <input
+          name="cigarette"
+          ref={cigaretteInput}
+          value={state.cigarette}
+          placeholder="종류를 입력해주세요."
+          onChange={onChange}
+        />
+      </div>
+      <div>
+        <input
+          name="price"
+          ref={priceInput}
+          value={state.price}
+          type="number"
+          placeholder="가격을 입력해주세요."
+          onChange={onChange}
+        />
+      </div>
+      <div>
+        <button onClick={onSubmit}>저장</button>
+      </div>
+    </div>
+  );
+};
+export default Settings;

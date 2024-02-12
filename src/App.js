0e05bd1877b719ad1cb6ccf47d76af5262a30397
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2024-02-13 03:41:29 +0900

    Router 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import "./App.css";
-
-function App() {
-  return <>App</>;
+import "./styles/App.css";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Settings from "./screens/Settings";
+import Report from "./screens/Report";
+import Home from "./screens/Home";
+import Diary from "./screens/Diary";
+import Place from "./screens/Place";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+function App(title) {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/report" element={<Report />} />
+          <Route path="/diary" element={<Diary />} />
+          <Route path="/place" element={<Place />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+        <Nav />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;

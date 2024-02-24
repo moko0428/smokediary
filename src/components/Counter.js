db56d964888f1fc607df5b857a197847c1855edb
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2024-02-24 21:29:44 +0900

    feat: count btn 3초 딜레이

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,12 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { SmokeDispatchContext } from "../App";
+import Loading from "./Loading";
 
 const Counter = () => {
   const { onCreate } = useContext(SmokeDispatchContext);
 
   const [data, setData] = useState(0);
+  const [isClick, setIsClick] = useState(false);
 
   const date = new Date();
 
@@ -12,11 +14,18 @@ const Counter = () => {
     const created_date = date.getTime();
     onCreate(created_date, data + 1);
     setData(data + 1);
+    setIsClick(true);
   };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsClick(false);
+    }, 3000);
+  }, [isClick]);
 
   return (
     <div className="Home_button Home_wrapper_column">
-      <button onClick={Counter}>+</button>
+      {isClick ? <Loading /> : <button onClick={Counter}>+</button>}
     </div>
   );
 };
---
+++ src/components/Loading.js
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="Loading_wrapper">
+      <div className="Loading"></div>
+    </div>
+  );
+};
+
+export default Loading;

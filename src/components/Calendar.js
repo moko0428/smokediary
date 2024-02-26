c057235cbd4a75fb153b254de4b5c459680574c1
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2024-02-26 21:26:35 +0900

    feat: date-fns 라이브러리 사용, 흡연 다이어리 레이아웃 구현

---
+++ src/components/Calendar.js
@@ -0,0 +1,130 @@
+import React, { useState } from "react";
+import { format, addMonths, subMonths } from "date-fns";
+import { startOfMonth, endOfMonth, startOfWeek, endOfWeek } from "date-fns";
+import { isSameMonth, isSameDay, addDays, parse } from "date-fns";
+
+const RenderHeader = ({ currentMonth, prevMonth, nextMonth }) => {
+  return (
+    <div className="Calendar_header row">
+      <div>
+        <span className="Calendar_text">
+          <span className="Calendar_text month">
+            {format(currentMonth, "M")}월
+          </span>
+          {format(currentMonth, "yyyy")}
+        </span>
+      </div>
+      <div className="Calendar_button col-end">
+        <button onClick={prevMonth}>{"<"}</button>
+        <button onClick={nextMonth}>{">"}</button>
+      </div>
+    </div>
+  );
+};
+
+const RenderDays = () => {
+  const days = [];
+  const date = [
+    "일요일",
+    "월요일",
+    "화요일",
+    "수요일",
+    "목요일",
+    "금요일",
+    "토요일",
+  ];
+
+  for (let i = 0; i < 7; i++) {
+    days.push(
+      <div className="col" key={i}>
+        {date[i]}
+      </div>
+    );
+  }
+
+  return <div className="Calendar_days row">{days}</div>;
+};
+
+const RenderCells = ({ currentMonth, selectedDate, onDateClick }) => {
+  const monthStart = startOfMonth(currentMonth);
+  const monthEnd = endOfMonth(monthStart);
+  const startDate = startOfWeek(monthStart);
+  const endDate = endOfWeek(monthEnd);
+
+  const rows = [];
+  let days = [];
+  let day = startDate;
+  let formattedDate = "";
+
+  while (day <= endDate) {
+    for (let i = 0; i < 7; i++) {
+      formattedDate = format(day, "d");
+      const cloneDay = day;
+      days.push(
+        <div
+          className={`col cell ${
+            !isSameMonth(day, monthStart)
+              ? "disabled"
+              : isSameDay(day, selectedDate)
+              ? "selected"
+              : format(currentMonth, "M") !== format(day, "M")
+              ? "not-valid"
+              : "valid"
+          }`}
+          key={day}
+          onClick={() => onDateClick(parse(cloneDay))}
+        >
+          <span
+            className={
+              format(currentMonth, "M") !== format(day, "M")
+                ? "text not-valid"
+                : ""
+            }
+          >
+            {formattedDate}
+          </span>
+        </div>
+      );
+      day = addDays(day, 1);
+    }
+    rows.push(
+      <div className="row" key={day}>
+        {days}
+      </div>
+    );
+    days = [];
+  }
+  return <div className="Calendar_body">{rows}</div>;
+};
+
+const Calender = () => {
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const prevMonth = () => {
+    setCurrentMonth(subMonths(currentMonth, 1));
+  };
+  const nextMonth = () => {
+    setCurrentMonth(addMonths(currentMonth, 1));
+  };
+  const onDateClick = (day) => {
+    setSelectedDate(day);
+  };
+  return (
+    <div className="Calendar">
+      <RenderHeader
+        prevMonth={prevMonth}
+        currentMonth={currentMonth}
+        nextMonth={nextMonth}
+      />
+      <RenderDays />
+      <RenderCells
+        currentMonth={currentMonth}
+        selectedDate={selectedDate}
+        onDateClick={onDateClick}
+      />
+    </div>
+  );
+};
+
+export default Calender;
--- src/routes/Diary.js
+++ src/routes/Diary.js
@@ -1,29 +1,36 @@
+import Calendar from "../components/Calendar";
 import Header from "../components/Header";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Diary = () => {
-  const [curDate, setCurDate] = useState(new Date());
-  const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+  // const [curDate, setCurDate] = useState(new Date());
+  // const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
 
-  const IncreaseMonth = () => {
-    setCurDate(
-      new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate())
-    );
-  };
-  const DecreaseMonth = () => {
-    setCurDate(
-      new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
-    );
-  };
+  // const IncreaseMonth = () => {
+  //   setCurDate(
+  //     new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate())
+  //   );
+  // };
+  // const DecreaseMonth = () => {
+  //   setCurDate(
+  //     new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
+  //   );
+  // };
+
+  useEffect(() => {
+    const titleElement = document.getElementsByTagName("title")[0];
+    titleElement.innerHTML = `연흡 | 다이어리`;
+  }, []);
   return (
     <div>
       <Header title={"흡연 다이어리"} />
       <div>
-        <div>
+        {/* <div>
           <button onClick={DecreaseMonth}>{"<"}</button>
           <div>{headText}</div>
           <button onClick={IncreaseMonth}>{">"}</button>
-        </div>
+        </div> */}
+        <Calendar />
       </div>
     </div>
   );
